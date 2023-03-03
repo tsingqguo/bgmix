9c91968e171b448edc3fa6f94b8b33d41972926d
rfww <68590186+rfww@users.noreply.github.com>
2023-03-03 10:14:41 +0800

    file update

--- models/models.py
+++ models/models.py
@@ -1,11 +1,8 @@
 import torch.nn as nn
 import torch
-from models.DG import GeneratorD, Discriminator2, Discriminator2C
-from models.TG import GeneratorT
-from models.CG import GeneratorVGGC, DiscriminatorCU, WSCD, WSCD2, WSCD4,WSCD5, DiscriminatorF, WSCD2_sub, WSCD2_ViT, WSCD2_Swin
-####################################################
-# Initialize generator and discriminator
-####################################################
+
+from models.CG import WSCD2, Discriminator2
+
 def Create_nets(args):
     # generator = GeneratorD()
     generator = WSCD2()
@@ -19,16 +16,12 @@ def Create_nets(args):
         discriminator = discriminator.cuda()
         discriminator2= discriminator2.cuda()
         # discriminator3=discriminator3.cuda()
-    if args.epoch_start != 0:
+    if args.resume:
         # Load pretrained models
-        generator.load_state_dict(torch.load(args.resume), False)
-        # generator.load_state_dict(torch.load('%sgenerator_best.pth' % (args.resume)), False)
-        # discriminator.load_state_dict(torch.load('%sdiscriminator1_best.pth' % (args.resume)))
-        # discriminator2.load_state_dict(torch.load('%sdiscriminator2_best.pth' % (args.resume)))
-        # generator.load_state_dict(torch.load('log/%s-%s/%s/generator_%d.pth' % (args.exp_name, args.dataset_name,args.model_result_dir,args.epoch_start)))
-        # discriminator.load_state_dict(torch.load('log/%s-%s/%s/discriminator_%d.pth' % (args.exp_name, args.dataset_name,args.model_result_dir,args.epoch_start)))
-        # discriminator2.load_state_dict(torch.load('log/%s-%s/%s/discriminator2_%d.pth' % (
-        # args.exp_name, args.dataset_name, args.model_result_dir, args.epoch_start)))
+        generator.load_state_dict(torch.load('%s/generator_best.pth' % (args.model_result_dir)))
+        discriminator.load_state_dict(torch.load('%s/discriminator1_best.pth' % (args.model_result_dir)))
+        discriminator2.load_state_dict(torch.load('%s/discriminator2_best.pth' % (args.model_result_dir)))
+
 
     return generator, discriminator,discriminator2
     # return generator, discriminator,discriminator2,discriminator3
